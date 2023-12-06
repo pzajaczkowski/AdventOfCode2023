f0cfa561c123dc251e29cfba109b0e86aaa36808
pzajaczkowski <demoon.pps@gmail.com>
2023-12-06 22:20:15 +0100

    Day4Part1

---
+++ Day4/Day4.cs
@@ -0,0 +1,27 @@
+﻿namespace Day4;
+
+public static class Day4
+{
+    public static void Exec()
+    {
+        var lines = File.ReadAllLines(@"../../../input.txt");
+        var sum = lines.Sum(GetCardValue);
+        Console.WriteLine(sum);
+    }
+    
+    private static int GetCardValue(string line)
+    {
+        var cardAndNumbers = line.Split(":");
+        var winningAndMyNumbers = cardAndNumbers[1].Split('|');
+        var winningNumbersCount = GetYourWinningNumbersCount(winningAndMyNumbers[0].Trim(), winningAndMyNumbers[1].Trim());
+        var result = (int)Math.Pow(2, winningNumbersCount - 1);
+        return result;
+    }
+
+    private static int GetYourWinningNumbersCount(string winningNumbers, string yourNumbers)
+    {
+        var winNumbers = winningNumbers.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        var numbers = yourNumbers.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        return numbers.Count(number => winNumbers.Contains(number));
+    }
+}
