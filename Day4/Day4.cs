d5f39ecb31b1e95e0f33fe87f33c9ae3c42c8a64
pzajaczkowski <demoon.pps@gmail.com>
2023-12-06 22:58:09 +0100

    Day4Part4

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -4,18 +4,24 @@ public static class Day4
 {
     public static void Exec()
     {
-        var lines = File.ReadAllLines(@"../../../input.txt");
-        var sum = lines.Sum(GetCardValue);
-        Console.WriteLine(sum);
+        var lines = File.ReadAllLines("../../../input.txt");
+        var cardsInstances = Enumerable.Repeat(1, lines.Length).ToArray();
+        foreach (var item in lines.Select((line, i) => ( line, i )))
+        {
+            CalculateCardValue(item.line, item.i, cardsInstances);
+        }
+        Console.WriteLine(cardsInstances.Sum());
     }
     
-    private static int GetCardValue(string line)
+    private static void CalculateCardValue(string line, int index, int[] cardsInstances)
     {
         var cardAndNumbers = line.Split(":");
         var winningAndMyNumbers = cardAndNumbers[1].Split('|');
         var winningNumbersCount = GetYourWinningNumbersCount(winningAndMyNumbers[0].Trim(), winningAndMyNumbers[1].Trim());
-        var result = (int)Math.Pow(2, winningNumbersCount - 1);
-        return result;
+        for (var i = 1; i <= winningNumbersCount; i++)
+        {
+            cardsInstances[index + i] += cardsInstances[index];
+        }
     }
 
     private static int GetYourWinningNumbersCount(string winningNumbers, string yourNumbers)
